2ebf96874f2c9e17f1952b52fb47d68e584efaac
Yagdrassyl <yagdrassyl@pop-os.localdomain>
2021-03-30 21:17:34 -0600

    Update

---
+++ Ejercicio2/.simple.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yagdrassyl/Documents/OS_Lab4/Ejercicio2/simple.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/yagdrassyl/Documents/OS_Lab4/Ejercicio2/simple.ko /home/yagdrassyl/Documents/OS_Lab4/Ejercicio2/simple.o /home/yagdrassyl/Documents/OS_Lab4/Ejercicio2/simple.mod.o;  true
---
+++ Ejercicio2/simple.mod
@@ -0,0 +1,2 @@
+/home/yagdrassyl/Documents/OS_Lab4/Ejercicio2/simple.o
+
